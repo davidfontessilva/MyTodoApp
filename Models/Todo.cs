cb8090cec9fca1dde974a917cb85edc7312cbde7
davidfontessilva <davidfontessilva01@gmail.com>
2025-12-31 11:34:16 -0300

    Initial project structure

---
+++ Models/Todo.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyTodoApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Título")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Title { get; set; }
+
+        [DisplayName("Concluído")]
+        public bool Done { get; set; }
+
+        [DisplayName("Criado em")]
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+
+        [DisplayName("Última atualização")]
+        public DateTime DateUpdated { get; set; } = DateTime.Now;
+    }
+}
